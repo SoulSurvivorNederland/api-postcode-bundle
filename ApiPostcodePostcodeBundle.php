5d4f2f0b70d2bcfb4d8c59cc494a0eb6f06632d9
Api Postcode <info@api-postcode.nl>
2017-06-05 17:15:06 +0200

    Create ApiPostcodePostcodeBundle.php

---
+++ ApiPostcodePostcodeBundle.php
@@ -0,0 +1,21 @@
+<?php
+/*
+* (c) Api Postcode <info@api-postcode.nl>
+*
+* For the full copyright and license information, please view the LICENSE
+* file that was distributed with this source code.
+*/
+
+namespace ApiPostcode\PostcodeBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Class ApiPostcodePostcodeBundle
+ *
+ * (c) Api Postcode <info@api-postcode.nl>
+ */
+class ApiPostcodePostcodeBundle extends Bundle
+{
+
+}
